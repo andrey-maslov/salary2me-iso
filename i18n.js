7edaa4928e0c0a2229332471b9a0b002eacb0649
Andrey Maslov <andrei.maslau@gmail.com>
2021-01-19 17:43:20 +0300

    language detector added / not works

--- i18n.js
+++ i18n.js
@@ -5,24 +5,19 @@ const path = require('path')
 module.exports = new NextI18Next({
     defaultLanguage: 'ru',
     otherLanguages: ['en', 'pl'],
-    // localeSubpaths: {
-    //     en: 'en',
-    //     ru: 'ru',
-    //     pl: 'pl'
-    // },
-    // fallbackLng: "en",
-    // serverLanguageDetection: false,
-    // browserLanguageDetection: false,
-    // detection: {
-    //     // check if language is cached in cookies, if not check local storage
-    //     order: ["cookie", "localStorage"],
-    //
-    //     // next-i18next by default searches for the "next-i18next" cookie on server requests
-    //     lookupCookie: "next-i18next",
-    //     lookupLocalStorage: "i18nextLng",
-    //
-    //     // cache the language in cookies and local storage
-    //     caches: ["cookie", "localStorage"]
-    // },
-    localePath:	path.resolve('public/locales'),
+    fallbackLng: 'ru',
+    serverLanguageDetection: false,
+    browserLanguageDetection: false,
+    detection: {
+        // check if language is cached in cookies, if not check local storage
+        order: ['cookie', 'localStorage'],
+
+        // next-i18next by default searches for the "next-i18next" cookie on server requests
+        lookupCookie: 'next-i18next',
+        lookupLocalStorage: 'i18nextLng',
+
+        // cache the language in cookies and local storage
+        caches: ['cookie', 'localStorage']
+    },
+    localePath: path.resolve('public/locales')
 })
--- src/actions/api/tafirrsAPI.ts
+++ src/actions/api/tafirrsAPI.ts
@@ -7,7 +7,10 @@ export async function fetchTariffsData(): Promise<ITariff[] | null> {
     try {
         const response = await axios(`${tariffsApiUrl}/list`)
         return response.data
-    } catch {
+    } catch (err) {
+        if (err.response && err.response.status) {
+            return err.response.status
+        }
         return null
     }
 }
