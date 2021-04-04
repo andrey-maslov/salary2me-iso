9ae400be58f39bd966fc21cea654b815638b6ee6
Andrey Maslov <andrei.maslau@gmail.com>
2021-04-04 23:20:22 +0300

    GTM put to correct place

--- src/page-layouts/MetaCommon.tsx
+++ src/page-layouts/MetaCommon.tsx
@@ -6,6 +6,7 @@ type HomeMetaType = {
     t: any
 }
 
+// TODO not use this because all meta in Layout component
 const MetaCommon = ({ t }: HomeMetaType) => {
     return (
         <Head>
@@ -20,27 +21,6 @@ const MetaCommon = ({ t }: HomeMetaType) => {
             <meta name="twitter:description" content={t('main:meta.description')} />
             <meta name="twitter:site" content={HOST} />
             <meta name="twitter:image" content={`${HOST}'/img/social.jpg`} />
-            {process.env.PRODUCTION && (
-                <>
-                    {/* Google Tag Manager */}
-                    <script
-                        dangerouslySetInnerHTML={{
-                            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                                })(window,document,'script','dataLayer','GTM-MFLMKPZ');`
-                        }}
-                    />
-                    <noscript
-                        dangerouslySetInnerHTML={{
-                            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MFLMKPZ"
-                      height="0" width="0" style="display:none;visibility:hidden"></iframe>`
-                        }}
-                    />
-                    {/* End Google Tag Manager */}
-                </>
-            )}
         </Head>
     )
 }
