76827ec7ffce5da12d93d047197ab1f8bce53f15
Andrey Maslov <andrei.maslau@gmail.com>
2021-04-29 21:43:18 +0300

    test meta title changed

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import { useRouter } from 'next/router'
 import { withTranslation, i18n } from '@i18n'
 import Footer from './footer/Footer'
 import Header from './header/Header'
@@ -7,6 +8,22 @@ import Modals from './modals/Modals'
 import { accentColor, HOST, SITE_TITLE } from '../../constants/constants'
 
 function Layout({ children, t }) {
+    const { pathname } = useRouter()
+    const mode = pathname === '/test' ? 'test' : 'common'
+
+    const meta = {
+        test: {
+            title: t('test:page.meta_title'),
+            desc: t('test:page.meta_desc'),
+            url: `${HOST}/test`
+        },
+        common: {
+            title: t('common:meta.title'),
+            desc: t('common:meta.description'),
+            url: HOST
+        }
+    }
+
     return (
         <>
             <Head>
@@ -16,16 +33,16 @@ function Layout({ children, t }) {
                 <meta name="description" content={t('common:meta.description')} />
                 <meta property="og:site_name" content={SITE_TITLE} />
                 <meta property="og:type" content="website" />
-                <meta property="og:url" content={HOST} />
-                <meta property="og:title" content={`${t('common:meta.title')} - ${SITE_TITLE}`} />
-                <meta property="og:description" content={t('common:meta.description')} />
+                <meta property="og:url" content={meta[mode].url} />
+                <meta property="og:title" content={`${meta[mode].title} - ${SITE_TITLE}`} />
+                <meta property="og:description" content={meta[mode].desc} />
                 <meta property="og:image" content={`${HOST}/img/default.jpg`} />
                 <meta property="og:locale" content={i18n.language} />
                 <meta name="twitter:card" content="summary_large_image" />
-                <meta name="twitter:title" content={`${t('common:meta.title')} - ${SITE_TITLE}`} />
-                <meta name="twitter:description" content={t('common:meta.description')} />
+                <meta name="twitter:title" content={`${meta[mode].title} - ${SITE_TITLE}`} />
+                <meta name="twitter:description" content={meta[mode].desc} />
                 <meta name="twitter:site" content={SITE_TITLE} />
-                <meta name="twitter:url" content={HOST} />
+                <meta name="twitter:url" content={meta[mode].url} />
                 <meta name="twitter:image" content={`${HOST}/img/default.jpg`} />
                 <meta name="theme-color" content={accentColor} />
                 {process.env.PRODUCTION && (
--- src/page-layouts/test/test-main/TestMainLayout.tsx
+++ src/page-layouts/test/test-main/TestMainLayout.tsx
@@ -1,8 +1,8 @@
 import Head from 'next/head'
-import { withTranslation } from '@i18n'
+import { i18n, withTranslation } from '@i18n'
 import Layout from '../../../components/layout/Layout'
 import TestMainContent from './content/TestMainContent'
-import { SITE_TITLE } from '../../../constants/constants'
+import { HOST, SITE_TITLE } from '../../../constants/constants'
 
 const TestMainLayout: React.FC<{ t: any }> = ({ t }) => {
     return (
